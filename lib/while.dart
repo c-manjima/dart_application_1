b9a28cc04676a5e78f479a031d0a58ec2197aa9c
Manjima2002 <157273428+Manjima2002@users.noreply.github.com>
2024-03-30 23:06:59 +0530

    listnames

---
+++ lib/while.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> names = [
+    "hari",
+    "jith",
+    "john",
+    "jancy",
+  ];
+  int count = 0;
+  while (count < names.length) {
+    print(names[count]);
+    count = count + 1;
+  }
+}
